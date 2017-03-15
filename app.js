960f245567f25fc74b5652e10b1e53cc9e8277dc
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-03-15 17:38:55 +0100

    Established mongoose connection, added the secret route and the requested template

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-var path    = require('path');
-var express = require('express');
-var app     = express();
+var path     = require('path');
+var mongoose = require('mongoose');
+
+var express  = require('express');
+var app      = express();
+
+//mongoose connection
+mongoose.connect('mongodb://localhost/userAuth');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
    res.render('home');
+});
+
+app.get('/secret', function(req, res){
+    res.render('secret');
 });
 
 app.listen(3000, function(){
